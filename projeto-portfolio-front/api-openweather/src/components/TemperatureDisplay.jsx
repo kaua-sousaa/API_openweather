50c74f9e130b483d5047908228dfd31a1bb770e0
kaua-sousaa <kauasousa15_rj@hotmail.com>
2025-07-09 15:34:22 -0300

    criando um componente para a temperatura e adicionado loading/error

--- projeto-portfolio-front/api-openweather/src/components/TemperatureDisplay.jsx
+++ projeto-portfolio-front/api-openweather/src/components/TemperatureDisplay.jsx
@@ -1,12 +1,12 @@
 const TemperatureDisplay = ({ label, weatherData, tempPrincipal = false }) => {
   const divFontPrincipal = "text-slate-500";  
   const pPrincipal = "text-7xl font-extrabold";
-  const pSpamPrincipal = "text-3xl";
+  const pSpamPrincipal = "text-xl";
 
   return (
     <div className={`${tempPrincipal ? divFontPrincipal : 'text-color-500'}`}>
       <p className={`${tempPrincipal ? pPrincipal : 'text-4xl font-extrabold'}`}>
-        {weatherData}{" "}<span className={`"align-top ${tempPrincipal ? pSpamPrincipal: "text-2xl"} font-medium text-slate-600"`}>°C</span>
+        {weatherData}<span className={`"${tempPrincipal ? pSpamPrincipal: "text-sm"} font-normal text-slate-600"`}>°C</span>
       </p>
       <p>{label}</p>
     </div>
--- projeto-portfolio-front/api-openweather/src/page/HomePage.jsx
+++ projeto-portfolio-front/api-openweather/src/page/HomePage.jsx
@@ -1,21 +1,17 @@
-import { Wind } from 'lucide-react';
 import { useState } from 'react';
 import SearchForm from '../components/SearchForm';
 import { searchCity } from '../services/searchCityService';
 import TemperatureDisplay from '../components/TemperatureDisplay';
 
 const HomePage = () => {
-    const [weatherData, setWeatherData] = useState({
-        city: '',
-        description: '',
-        temperature: 0,
-        temperature_max: 0,
-        temperature_min: 0,
-        feelsLike: 0,
-        icon: ''
-    })
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [weatherData, setWeatherData] = useState(null)
 
     const handleSearch = async (city) => {
+        setLoading(true);
+        setError(null);
+        setWeatherData(null);
         try {
             const data = await searchCity(city);
             console.log("Dados do clima:", data);
@@ -32,7 +28,9 @@ const HomePage = () => {
             }
         } catch (error) {
             console.error("Erro ao buscar dados do clima:", error);
-            alert("Não foi possível obter os dados do clima. Tente novamente.");
+            setError("Não foi possível encontrar a cidade. Por favor, verifique o nome e tente novamente.");
+        } finally{
+            setLoading(false);
         }
     }
 
@@ -40,22 +38,32 @@ const HomePage = () => {
         <div className="flex min-h-screen items-center justify-center bg-gradient-to-br from-sky-50 to-slate-200 p-4">
             <div className="w-1/3 rounded-xl bg-white/70 p-8 shadow-lg backdrop-blur-lg border border-slate-200">
                 <SearchForm onSubmit={handleSearch}/>
-
-                <div className="mt-8 flex flex-col items-center gap-4 text-center">
-                    <div className="text-7xl">                       
-                        <img src={`https://openweathermap.org/img/wn/${weatherData.icon}@2x.png`} alt="" />
+                {loading && <p className="text-sky-600">Buscando...</p>}
+                {error && <p className="text-red-600">{error}</p>}
+                
+                {!loading && !error && !weatherData && (
+                    <div className="mt-8 text-center">
+                        <p className="text-lg text-slate-600">Digite o nome de uma cidade para ver o clima.</p>
                     </div>
-                    <div>
-                        <h1 className="text-4xl font-bold text-slate-800">{weatherData.city}</h1>
-                        <p className="mt-1 text-lg text-slate-500">{weatherData.description}</p>
+                )}
+                {weatherData && (
+                    <div className="mt-8 flex flex-col items-center gap-4 text-center">          
+                        <div className="text-7xl">                       
+                            <img src={`https://openweathermap.org/img/wn/${weatherData.icon}@2x.png`} alt="" />
+                        </div>
+                        <div>
+                            <h1 className="text-4xl font-bold text-slate-800">{weatherData.city}</h1>
+                            <p className="mt-1 text-lg text-slate-500">{weatherData.description}</p>
+                        </div>
+                        
+                        <div className='flex items-center space-x-14'>
+                            <TemperatureDisplay label="Mínima" weatherData={weatherData.temperature_min}/>
+                            <TemperatureDisplay label="Atual"weatherData={weatherData.temperature} tempPrincipal={true}/>
+                            <TemperatureDisplay label="Máxima" weatherData={weatherData.temperature_max}/>                      
+                        </div>
+                        <p className="text-lg text-slate-600">Sensação térmica: {weatherData.feelsLike}</p>
                     </div>
-                    <div className='flex items-center space-x-14'>
-                        <TemperatureDisplay label="Mínima" weatherData={weatherData.temperature_min}/>
-                        <TemperatureDisplay label="Atual"weatherData={weatherData.temperature} tempPrincipal={true}/>
-                        <TemperatureDisplay label="Máxima" weatherData={weatherData.temperature_max}/>                      
-                    </div>
-                    <p className="text-lg text-slate-600">Sensação térmica: {weatherData.feelsLike}</p>
-                </div>
+                )}
             </div>
         </div>
     );
