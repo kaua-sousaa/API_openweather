04442d78fb0dfd26a6a9d23bac4e02aa9373992a
kaua-sousaa <kauasousa15_rj@hotmail.com>
2025-07-22 22:46:37 -0300

    adicionando +1 endereco no CrossOrigin

--- projeto-portfolio/src/main/java/api/projeto_portfolio/controller/ClimaController.java
+++ projeto-portfolio/src/main/java/api/projeto_portfolio/controller/ClimaController.java
@@ -11,7 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 import api.projeto_portfolio.dto.WeatherDTO;
 import api.projeto_portfolio.services.ClimaService;
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = {"http://localhost:5173", 
+"https://6880365e6dd5960008b70560--api-clima-kaua-openweather.netlify.app"})
+
 @RestController
 @RequestMapping("/api/dados-clima")
 public class ClimaController {
