11995a1ca8385373f6c40fc5ebae30045a2cdcc8
kaua-sousaa <kauasousa15_rj@hotmail.com>
2025-07-22 22:09:40 -0300

    mudando a URL para modo producao, saindo de localhost

--- projeto-portfolio-front/api-openweather/src/services/searchCityService.js
+++ projeto-portfolio-front/api-openweather/src/services/searchCityService.js
@@ -1,8 +1,13 @@
-const API_BASE_URL = "http://localhost:8080";
+//const API_BASE_URL = "http://localhost:8080";
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 import axios from 'axios';
 
 export const searchCity = async (cityName) => {
     try {
+        if (!API_BASE_URL){
+            console.error("API_BASE_URL não está definido. Verifique as variáveis de ambiente.");
+            throw new Error("API_BASE_URL não está definido.");
+        }
         const apiURL = `${API_BASE_URL}/api/dados-clima/${cityName}`
 
         const response = await axios.get(apiURL)
